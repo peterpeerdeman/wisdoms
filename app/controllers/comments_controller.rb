3837c23dfeb15123247889f0c8dafe7da47a51ba
Peter Peerdeman <peter@peterpeerdeman.nl>
2013-09-16 16:40:15 +0200

    updated forms, added validations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,28 +2,28 @@ class CommentsController < ApplicationController
   before_filter :require_user, :except => [:show, :index]
 
   def index
-    @wisdom = Wisdom.find(params[:wisdom_id])
+    @wisdom = Wisdom.friendly.find(params[:wisdom_id])
     render :json => @wisdom.comments
   end
 
   def show
-    @comment = Comment.find(params[:id])
+    @comment = Comment.friendly.find(params[:id])
     render :json => @comment
   end
 
   def create
-    @wisdom = Wisdom.find(params[:wisdom_id])
+    @wisdom = Wisdom.friendly.find(params[:wisdom_id])
     @comment = @wisdom.comments.create(params[:comment].permit(:body))
     @comment.user_id = current_user.id
     if @comment.save
       redirect_to @wisdom
     else
-      render 'new'
+      redirect_to wisdom_path(@wisdom)
     end
   end
 
   def destroy
-    @wisdom = Wisdom.find(params[:wisdom_id])
+    @wisdom = Wisdom.friendly.find(params[:wisdom_id])
     @comment = @wisdom.comments.find(params[:id])
     @comment.destroy
     redirect_to wisdom_path(@wisdom)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :wisdom
 
+  validates :body, presence:true,
+                    length: { minimum: 1 }
   self.per_page = 30
 end
