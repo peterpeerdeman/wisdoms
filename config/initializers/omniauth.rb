c573effe39473ca30efd2beddd22f76338669fd9
Peter Peerdeman <peter@peterpeerdeman.nl>
2013-09-18 11:30:42 +0200

    removed post to facebook checkbox, replaced with send button

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['WISDOMS_FB_APP_ID'],ENV['WISDOMS_FB_APP_SECRET'],:scope => 'email,publish_actions'
+  provider :facebook, ENV['WISDOMS_FB_APP_ID'],ENV['WISDOMS_FB_APP_SECRET'],:scope => 'email'
 end
